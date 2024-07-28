d5f157c945390e970af4631b25b31515ad51b052
moolmin <zzmzzm0065@gmail.com>
2024-07-28 15:18:30 +0900

    chore add info in homa page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import styles from './Home.module.css';
 import { useNavigate } from 'react-router-dom';
 import CardItem from "../components/card/CardItem.tsx";
 // import Footer from "../components/layout/Footer.tsx";
+import { FaBoltLightning } from "react-icons/fa6";
+import {Popover} from 'antd';
 
 const categories = [
     { id: 1, name: '밥', imgSrc: '/category/1.png', path: '/post/밥' },
@@ -17,8 +19,23 @@ const categories = [
 
 
 const Home: React.FC = () => {
+    // const [arrow, setArrow] = useState('Show');
 
     const navigate = useNavigate();
+
+    // const mergedArrow = useMemo(() => {
+    //     if (arrow === 'Hide') {
+    //         return false;
+    //     }
+    //
+    //     if (arrow === 'Show') {
+    //         return true;
+    //     }
+    //
+    //     return {
+    //         pointAtCenter: true,
+    //     };
+    // }, [arrow]);
 
     const handleCategoryClick = (path: string) => {
         navigate(path);
@@ -26,6 +43,11 @@ const Home: React.FC = () => {
 
     return (
         <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <Popover overlayStyle={{ width: '300px' }}  placement="bottomRight" title={"번게더란?"} content={"카클스를 위한 카클스에 의한 소모임 플랫폼입니다! 수강생 50명 모두가 원하는 소모임을 만들고 참여할 수 있는 장을 만들고 싶었어요. 자유롭게 번개를 생성하고 참여하되, 오픈채팅방은 번개 종료시 이용하지 않도록 합시다! ⚡️"}>
+                <FaBoltLightning className={styles.info} />
+                </Popover>
+            </div>
             <div className={styles.logoContainer}>
                 <img src="/logo.png" alt="Logo"/>
             </div>
